a6de6e93e363a2da21ba2fbb5fe881e64d083883
vf <vf>
2023-07-16 00:59:19 +0200

    fix

--- src/Engine/ScrapingController.js
+++ src/Engine/ScrapingController.js
@@ -72,7 +72,6 @@ class ScrapingController {
         this._scheduleScraping(SourcesIndex.BolognaTodayITHome);
         this._scheduleScraping(SourcesIndex.IlRestoDelCarlinoITBologna);
         this._scheduleScraping(SourcesIndex.CorriereITSiteSearchBologna);
-        this._handle(new CronJob('* * * * * *'), SourcesIndex.CorriereITSiteSearchBologna);
     }
 }
 
