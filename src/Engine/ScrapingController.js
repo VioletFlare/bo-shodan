06f233f6015bb33f1378f4abe22a2bc154006d61
vf <vf>
2023-08-11 02:42:28 +0200

    temporary disable due to puppeteer memory leak

--- src/Engine/ScrapingController.js
+++ src/Engine/ScrapingController.js
@@ -95,7 +95,7 @@ class ScrapingController {
         this._scheduleScraping(SourcesIndex.BolognaNotizieCOM);
         this._scheduleScraping(SourcesIndex.Bologna24OreIT);
         this._scheduleScraping(SourcesIndex.ComuneBolognaITAjaxJson);
-        this._scheduleScraping(SourcesIndex.RainewsITHome);
+        //this._scheduleScraping(SourcesIndex.RainewsITHome);
     }
 }
 
