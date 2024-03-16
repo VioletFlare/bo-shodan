92812205289e2da9c2fd701bb5e67e8e9f4bf6a3
vf <violetflare@users.noreply.github.com>
2024-03-16 18:15:28 +0100

    Localizer and HotThemes

--- src/Engine/ArticleContextAnalyzer.js
+++ src/Engine/ArticleContextAnalyzer.js
@@ -7,7 +7,7 @@ class ArticleContextAnalyzer {
         this.config = this._initConfiguration(config);
     }
 
-    _shouldRun(url) {
+    _shouldLocalize(url) {
         this.isLocalizerEnabled = this._isEnabledFor('localizer', url);
 
         return this.isLocalizerEnabled;
@@ -43,46 +43,77 @@ class ArticleContextAnalyzer {
             tags += tag.toLowerCase() + " ";
         });
 
-        const context = title + " " + description + " " + tags;
+        const normal = title + " " + description + " " + tags;
+        const splitted = normal.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").split(" ");
 
-        return context;
+        return { normal, splitted };
     }
 
-    _run(articles) {
-        const filteredArticles = [];
+    _isLocal(context) {
+        let isLocal = false;
+
+        LocalizerData.some(localizerValue => {
+            isLocal = context.normal.includes(localizerValue);
+
+            if (isLocal) {
+                return true;
+            }
+        })
         
-        articles.forEach((article) => {
-            let isValid = false;
+        return isLocal;
+    }
 
-            const context = this._extractContext(article);
+    _isHot(context) {
+        let isHot = false;
 
-            LocalizerData.some(localizerValue => {
-                isValid = context.includes(localizerValue);
+        HotThemes.some(hotTheme => {
+            isHot = context.normal.includes(hotTheme);
 
-                if (isValid) {
+            if (isHot) {
+                return true;
+            }
+        })
+
+        if (!isHot) {
+            HotThemesWords.some(hotThemeWord => {
+                isHot = context.splitted.includes(hotThemeWord);
+
+                if (isHot) {
                     return true;
                 }
             })
+        }
 
-            if (isValid) {
-                filteredArticles.push(article);
+        return isHot;
+    }
+
+    _run(articles, options) {
+        const modifiedArticles = [];
+        
+        articles.forEach((article) => {
+            const context = this._extractContext(article);
+            const isLocal = this._isLocal(context) || !options.shouldLocalize;
+            const isHot = this._isHot(context);
+
+            if (isLocal) {
+                article.metaIsHot = isHot;
+
+                modifiedArticles.push(article)
             }
         });
 
-        return filteredArticles;
+        return modifiedArticles;
     }
 
-run(sourceUrl, articles) {
-    const shouldRun = this._shouldRun(sourceUrl);
+    run(sourceUrl, articles) {
+        const options = {
+            shouldLocalize: this._shouldLocalize(sourceUrl)
+        };
 
-    if (shouldRun) {
-        const filteredArticles = this._run(articles);
+        const modifiedArticles = this._run(articles, options);
 
-        return filteredArticles;
-    } else {
-        return articles;
+        return modifiedArticles;
     }
-}
 
 }
 
